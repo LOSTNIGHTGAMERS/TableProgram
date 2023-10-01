fa1178712bccf775dfcc92c18d55f86ae714241a
Navraj <lostnightgamers@gmail.com>
2023-10-01 16:28:45 +0530

    first commit

---
+++ TableProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TableProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Table table = new Table();
+            table.Calculte(5);
+        }
+    }
+}
---
+++ TableProgram/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableProgram
+{
+    public class Table
+    {
+        public void Calculte(int tableNumber)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                //Console.WriteLine($"{tableNumber} * {i} = {tableNumber * i}");
+                Console.WriteLine(tableNumber + " * " + i + " = " + tableNumber * i);
+            }
+        }
+    }
+}
